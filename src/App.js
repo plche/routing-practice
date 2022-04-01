ea6c621156cb6d763e93dd8209897b75ec92447a
plche <che.percy.l@gmail.com>
2022-04-01 16:56:37 -0500

    Using props.match.params

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch, useParams} from "react-router-dom";
+// import {BrowserRouter as Router, Route, Switch, useParams} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const Home = () => <h2 className="Home">Welcome</h2>;
 
-const Param = () => {
-    let {id} = useParams();
+const Param = (props) => {
+    // let {id} = useParams();
+    const {id} = props.match.params;
     if (isNaN(id)) return <h2 className="Param">The word is: {id}</h2>
     else return <h2 className="Param">The number is: {id}</h2>
 }
 
-const MoreParam = () => {
-    let {id, bgColor, color} = useParams();
+const MoreParam = (props) => {
+    // let {id, bgColor, color} = useParams();
+    const {id, bgColor, color} = props.match.params;
     if (isNaN(id)) return <h2 className="MoreParam" style={{backgroundColor: bgColor, color: color}}>The word is: {id}</h2>
 }
 
@@ -18,15 +21,18 @@ const App = () => (
     <Router>
         <div className="App">
             <Switch>
-                <Route path="/home">
+                {/*<Route path="/home">
                     <Home />
-                </Route>
-                <Route path="/:id/:color/:bgColor">
+                </Route>*/}
+                <Route path="/home" render={Home}/>
+                {/*<Route path="/:id/:color/:bgColor">
                     <MoreParam />
-                </Route>
-                <Route path="/:id">
+                </Route>*/}
+                <Route path="/:id/:color/:bgColor" render={routeProps => <MoreParam {...routeProps} />} />
+                {/*<Route path="/:id">
                     <Param />
-                </Route>
+                </Route>*/}
+                <Route path="/:id" render={routeProps => <Param {...routeProps} />} />
             </Switch>
         </div>
     </Router>
